6c97e9a1a35793f1a761f4c87aabc4e6a4f6ea2f
Abubakar Ali <abubakar@Abubakars-MacBook.local>
2020-12-19 08:54:09 +0300

    refractored login controller

--- routes/OfficeRoute.js
+++ routes/OfficeRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const OfficeController = require('./../controllers/OfficeController');
-const authController = require('./../controllers/authController');
+const OfficeController = require('../controllers/OfficeController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
